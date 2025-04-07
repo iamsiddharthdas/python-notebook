89ce2e7e1e7cc2c854d57fd603d422361d89cbf6
Siddharth <siddharthdas2203@gmail.com>
2025-04-07 11:46:54 +0530

    cs

---
+++ CS/replace_str.py
@@ -0,0 +1,5 @@
+message='Hello World'
+
+new_message= message.replace('World','Universe')
+
+print(new_message) # prints Hello 'Universe' instead of Hello 'World'
--- replace_str.py
+++
@@ -1,5 +0,0 @@
-message='Hello World'
-
-new_message= message.replace('World','Universe')
-
-print(new_message) # prints Hello 'Universe' instead of Hello 'World'
