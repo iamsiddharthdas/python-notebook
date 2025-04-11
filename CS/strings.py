38eebc526dd1df0b76057292751fd3f8d89b093c
Siddharth Das <siddharthdas2203@gmail.com>
2025-04-11 13:22:44 +0530

    strings

--- CS/strings.py
+++ CS/strings.py
@@ -9,7 +9,7 @@ print(len(message)) #length of string
 print(message[0]) # [] -> index, starts at 0. [0] -> first character i.e "B"
 print(message[0:5]) # 0th index is included & 5th index is excluded. so it will print "Bobby"
 print(message[:5]) # prints same thing as [0:5]
-print(message[8:]) #strats from 8th index i.e W and prints "World is a good cartoon in 1990s". 
+print(message[8:]) #starts from 8th index i.e W and prints "World is a good cartoon in 1990s". 
 
 print(message.lower()) # print the string in lowercase
 print(message.upper()) # print the string in uppercase
