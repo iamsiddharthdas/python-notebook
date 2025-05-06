6bbbad314f51fda7287e28005ab54490b694bb24
Siddharth Das <siddharthdas2203@gmail.com>
2025-05-06 22:18:13 +0530

    tribonacci

---  Concept Notebook/4. Functions/recursion.py
+++  Concept Notebook/4. Functions/recursion.py
@@ -816,3 +816,14 @@ KEY POINTS OF DIFFERENCE:
 
 """
 
+def tribonacci(n):
+    if n==0:
+        return 0
+    elif n==1 or n==2:
+        return 1
+    else:
+        return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
+    
+for i in range(8):
+    print(tribonacci(i))
+    
