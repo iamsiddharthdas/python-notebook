129699dfd8f80a5c6eeb9e8311d9fdf1d0bcfa45
Siddharth Das <siddharthdas2203@gmail.com>
2025-05-14 21:22:52 +0530

    practice

--- practice.py
+++
@@ -1,9 +0,0 @@
-x = ["Tomato", "Brinjal", "Okhra", "Cabbage", "Bottle Gourd", "Cauliflowerssss", "Snake Gourd", "Moringa"]
-
-longest = x[0]
-for i in x:
-    if len(i) > len(longest):
-        longest = i
-
-print(f"The longest vegetable name is: {longest}")
-
