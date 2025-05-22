d52ae0b8e959e57b96b146a60abc5c4077104496
Siddharth Das <siddharthdas2203@gmail.com>
2025-05-22 21:42:09 +0530

    dictionary

---  Concept Notebook/6. Dictionary/dictionary.py
+++  Concept Notebook/6. Dictionary/dictionary.py
@@ -227,3 +227,369 @@ Price --> 55.99
 
 '''
 
+# Iterating through Dictionary using for-zip loop
+
+x = [10,20,30,40,50]
+y = ["apple","mango","banana","grapes","orange"]
+
+for i,j in zip(x,y):
+    print(i,j)
+    
+'''
+Output:
+
+10 apple
+20 mango
+30 banana
+40 grapes
+50 orange
+
+
+'''
+
+x = [10, 20, 30, 40, 50]
+y = ["apple", "mango", "banana", "grapes", "orange"]
+z = [1.2, 1.5, 0.8, 2.0, 1.1]  # Price per unit
+
+for i, j, k in zip(x, y, z):
+    print(i, j, k)
+
+'''
+Output:
+10 apple 1.2
+20 mango 1.5
+30 banana 0.8
+40 grapes 2.0
+50 orange 1.1
+
+
+'''
+
+# Without dict
+
+''
+x = ["apple", "mango", "kiwi", "banana", "orange"]
+y = [10, 20, 30, 40, 50]
+
+z = {}
+
+for i,j in zip(x, y):
+    z[i] = j
+
+print(z)
+
+'''
+Output:
+
+{'apple': 10, 'mango': 20, 'kiwi': 30, 'banana': 40, 'orange': 50}
+
+
+'''
+
+# With dict
+
+x = ["apple", "mango", "kiwi", "banana", "orange"]
+y = [10, 20, 30, 40, 50]
+
+z = dict(zip(x, y))
+
+print(z)
+
+'''
+Output:
+
+{'apple': 10, 'mango': 20, 'kiwi': 30, 'banana': 40, 'orange': 50}
+
+
+'''
+
+# Turn it back into list
+
+z = {'apple': 10, 'mango': 20, 'kiwi': 30, 'banana': 40, 'orange': 50}
+x=[]
+y=[]
+for i in z:
+    x.append(i)
+    y.append(z[i])
+print (x) 
+print (y)
+
+'''
+Output:
+['apple', 'mango', 'kiwi', 'banana', 'orange']
+[10, 20, 30, 40, 50]
+
+'''
+
+
+# Make it into array and form keys and values
+
+z = {'apple': 10, 'mango': 20, 'kiwi': 30, 'banana': 40, 'orange': 50}
+
+x = []
+
+for i in z:
+    j = z[i]
+    x.append([i, j])
+
+print(x)
+
+
+"""
+Output:
+[['apple', 10], ['mango', 20], ['kiwi', 30], ['banana', 40], ['orange', 50]]
+
+"""
+
+# Update function
+
+x = {}
+
+x.update({"Raju": "Men"})
+x.update({"Sunita": "Women"})
+
+print(x)
+
+'''
+Output:
+
+{'Raju': 'Men', 'Sunita': 'Women'}
+
+
+'''
+
+# Delete a key
+
+x = {}
+
+x.update({"Raju": "Men"})
+x.update({"Sunita": "Women"})
+
+
+del x["Raju"]
+print(x)
+
+'''
+Output:
+
+{'Sunita': 'Women'}
+
+
+'''
+
+# pop function
+
+x = {}
+
+x.update({"Raju": "Men"})
+x.update({"Sunita": "Women"})
+
+x.pop("Raju")
+print(x)
+
+'''
+Output:
+
+{'Sunita': 'Women'}
+
+
+'''
+
+# pop item function - deletes the last item
+
+x = {}
+
+x.update({"Raju": "Men"})
+x.update({"Sunita": "Women"})
+
+x.popitem() # deletes the last item i.e. Sunita
+print(x)
+
+'''
+Output:
+
+{'Raju': 'Men'}
+
+'''
+
+# items function
+
+x = {}
+
+x.update({"Raju": "Men"})
+x.update({"Sunita": "Women"})
+
+print(x.items()) # converts the dictionary into a list by making keys and values into tuples
+
+'''
+Output:
+
+dict_items([('Raju', 'Men'), ('Sunita', 'Women')]) 
+
+'''
+
+# Swap keys and values
+
+x = {
+    "a": 100,
+    "b": 200,
+    "c": 300,
+    "d": 400
+}
+
+y = []
+
+for i in x:
+    j = x[i]
+    y.append([j, i])
+
+print(y)
+
+'''
+Output:
+
+[[100, 'a'], [200, 'b'], [300, 'c'], [400, 'd']]
+
+
+'''
+
+'''Another method'''
+x = {
+    "a": 100,
+    "b": 200,
+    "c": 300,
+    "d": 400
+}
+
+keys = x.keys()
+values = x.values()
+
+newDict = {}
+for i, j in zip(keys, values):
+    newDict[j] = i
+
+print(newDict)
+
+'''
+Output:
+
+[[100, 'a'], [200, 'b'], [300, 'c'], [400, 'd']]
+
+
+'''
+
+# Handles duplicates
+
+x = {
+    "a": 100,
+    "b": 200,
+    "c": 100
+}
+
+y = {}
+
+for i in x:
+    j = x[i]
+    if j not in y:
+        y[j] = [i]
+    else:
+        y[j].append(i)
+
+print(y)
+
+'''
+Output:
+{100: ['a', 'c'], 200: ['b']}
+
+
+
+'''
+
+'''Another way to handle duplication'''
+
+x = {
+    "a": 100,
+    "b": 200,
+    "c": 100
+}
+
+newDict = {}
+
+for i, j in zip(x.keys(), x.values()):
+    if j not in newDict:
+        newDict[j] = [i]
+    else:
+        newDict[j].append(i)
+
+print(newDict)
+
+'''
+Output:
+{100: ['a', 'c'], 200: ['b']}
+
+'''
+
+# Dictionary comprehension
+
+x = {i:i*2 for i in range(5)}
+
+print(x)
+
+'''
+Output:
+
+{0: 0, 1: 2, 2: 4, 3: 6, 4: 8}
+
+
+'''
+
+# Dictionary comprehension with if condition
+
+x = {i:i*2 for i in range(5) if i%2 == 0}
+
+print(x)
+
+'''
+Output:
+
+{0: 0, 2: 4, 4: 8}
+
+'''
+
+# making y as key and x as value using dictionary comprehension
+
+y = ["apple", "mango", "grapes", "kiwi", "orange"]
+x = [10, 20, 30, 40, 50]
+
+z = {j: i for i, j in zip(x,y)}
+
+print(z)
+
+'''
+Output:
+
+{'apple': 10, 'mango': 20, 'grapes': 30, 'kiwi': 40, 'orange': 50}
+
+
+'''
+
+# Multiple conditions in dictionary comprehension
+
+x = [i*j if i*j!=0 else False for i in range(3) for j in range(3)]
+
+print(x)
+
+'''
+
+This is what it does under the hood
+
+for i in range(3):
+    for j in range(3):
+        print(i,j)
+
+Output:
+
+[0, 0, 0, 0, 1, 2, 0, 2, 4]
+
+
+'''
